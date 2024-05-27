7256de3b42b6c341447b1976153a5945b8faccbf
Carol Mazutti <cpmazutti@gmail.com>
2024-05-27 18:01:46 -0300

    Listar produtos

--- src/front/service/produto.js
+++ src/front/service/produto.js
@@ -38,3 +38,74 @@ async function inserirProduto() {
         alert('Erro ao inserir produto');
     }
 }
+
+// -----------------------------------------------
+
+async function listaProduto() {
+    try {
+        const response = await fetch('http://localhost:3333/produto');
+        if (!response.ok) {
+            throw new Error('Erro ao buscar produtos');
+        }
+        const produtos = await response.json();
+        preencherTabela(produtos);
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Erro ao buscar produtos');
+    }
+}
+
+function preencherTabela(produtos) {
+    const tabela = document.getElementById('listaProduto').getElementsByTagName('tbody')[0];
+    tabela.innerHTML = ''; // Limpa a tabela antes de adicionar novos dados
+
+    produtos.forEach(produto => {
+        const linha = tabela.insertRow();
+        linha.insertCell(0).textContent = produto.id_produto;
+        linha.insertCell(1).textContent = produto.nome;
+        linha.insertCell(2).textContent = produto.descricao;
+        linha.insertCell(3).textContent = produto.preco;
+        linha.insertCell(4).textContent = produto.quantidade;
+        linha.insertCell(5).textContent = produto.fornecedor_produto_id;
+        linha.insertCell(6).innerHTML = '<input type="button" onClick="alterarProduto(produto)"/>' + "\n" + '<input type="button" onClick="excluirProduto()"/>';
+    });
+}
+
+async function alterarProduto(produto){
+    
+}
+
+async  function  excluirProduto(id_produto){
+    try {
+        const response = await fetch('http://localhost:3333/produto/excluir/' + id_produto);
+        if (!response.ok) {
+            throw new Error('Erro ao excluir produto');
+        }
+        const produtos = await response.json();
+        preencherTabela(produtos);
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Erro ao se comunicar com a rota produto/excluir');
+    }
+}
+
+
+// Chama a função buscarProdutos quando a página carrega
+window.onload = listaProduto;
+
+
+async function deleteProduto(id_produto) {
+    try {
+        const response = await fetch(`/api/products/${id_produto}`, {
+            method: 'DELETE'
+        });
+
+        if (response.ok) {
+            fetchProducts();
+        } else {
+            console.error('Falha ao deletar produto: ', response.statusText);
+        }
+    } catch (error) {
+        console.error('Erro ao deletar produto: ', error);
+    }
+}
